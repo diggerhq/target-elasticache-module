539fa44e5e1a63a7e9e3a21b721578200968420a
alexey <alexey@digger.dev>
2022-11-21 15:39:18 +0000

    add redis port and hostname as outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "redis_url" {
   value = var.redis_number_nodes > 1 ? "${aws_elasticache_replication_group.elasticache_replication_group[0].primary_endpoint_address}:${var.redis_port}" : "${aws_elasticache_cluster.elasticache_cluster[0].cache_nodes[0].address}:${var.redis_port}"
 }
+
+output "redis_hostname" {
+  value = var.redis_number_nodes > 1 ? aws_elasticache_replication_group.elasticache_replication_group[0].primary_endpoint_address : aws_elasticache_cluster.elasticache_cluster[0].cache_nodes[0].address
+}
+
+output "redis_port" {
+  value = var.redis_number_nodes > 1 ? aws_elasticache_replication_group.elasticache_replication_group[0].port : aws_elasticache_cluster.elasticache_cluster[0].cache_nodes[0].port
+}
